240496610358f6e416da81bebf7aee634b8940ef
Viktor Ermolov <viktor@ermolov.dev>
2021-03-03 20:17:03 +0000

    fix eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/extensions
+
 import hello from '../src/cli.js';
 
-// eslint-disable-next-line no-console
 console.log('Welcome to the Brain Games!');
 
 hello();
