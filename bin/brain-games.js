27e0db99c6f536a6eda16ec48184555849cd595b
Viktor Ermolov <viktor@ermolov.dev>
2021-03-03 19:51:02 +0000

    step 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+// eslint-disable-next-line import/extensions
+import hello from '../src/cli.js';
 
+// eslint-disable-next-line no-console
 console.log('Welcome to the Brain Games!');
+
+hello();
