3165bdd3a7c05108fb2986ec176f688e408f31d3
Viktor Ermolov <viktor@ermolov.dev>
2021-03-05 18:37:59 +0000

    fix structure

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import name from '../src/cli.js';
-import evenGame from '../src/even.js';
+import evenGame from '../src/games/even.js';
 
 console.log(`Hello, ${name}!`);
 evenGame(name);
